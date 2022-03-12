b75fe3feda8a1a16c80911cac1f9ec9793b0b603
pooja singh <poojasingh20@navgurukul.org>
2022-03-12 23:19:50 +0530

    deleted something

--- 02_randomUser/utils/getElement.js
+++
@@ -1,8 +0,0 @@
-// see if you can change here something😉
-const getElement = (selection) => {
-  const element = document.querySelector(selection);
-  if (element) return element;
-  throw new Error('no element selected');
-};
-
-export default getElement;
--- 02_randomUser/utils/removeActive.js
+++
@@ -1,4 +0,0 @@
-//You don't have to change anything here as well
-export default function removeActive(items) {
-  items.forEach((btn) => btn.classList.remove('active'));
-}
